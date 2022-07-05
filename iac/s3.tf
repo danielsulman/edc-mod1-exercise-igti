7354c927ddf2d9dc285c6dcab98f4bbe0ce32675
daniel <daniel.sulman@nyxsolucoes.com.br>
2022-07-05 19:06:03 -0300

    correção sintaxe

--- iac/s3.tf
+++ iac/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "datalake" {
 
   server_side_encryption_configuration{
     rule{
-	    apply_server_side_encryption_by_deafult{
+	    apply_server_side_encryption_by_default{
 	      sse_algorithm = "AES256"
 	    }
 	  }
